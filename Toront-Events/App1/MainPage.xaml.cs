08cd5c571cd985073fe00cbc9474eaacea3f5e03
Daniel Gen Li <facultyofmusic@gmail.com>
2013-05-27 13:19:45 -0400

    Initial commit.  Added project backbones

---
+++ Toront-Events/App1/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using Bing.Maps;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace App1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            InitializeMap();
+        }
+
+        void InitializeMap()
+        {
+            myMap.Center = new Location(43.77091, -79.41133);
+            myMap.ZoomLevel = 12;
+            myMap.MapType = MapType.Aerial;
+            myMap.Width = 600;
+            myMap.Height = 800;
+
+            Pushpin pushpin = new Pushpin();
+            pushpin.Text = "OMG ZE CUTE";
+            MapLayer.SetPosition(pushpin, new Location(43.77091, -79.41133));
+            myMap.Children.Add(pushpin);
+        }
+
+        private async void pushpinTapped(object sender, Windows.UI.Xaml.Input.TappedRoutedEventArgs e)
+        {
+            MessageDialog dialog = new MessageDialog("Hello from Seattle.");
+            await dialog.ShowAsync();
+        }
+
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.  The Parameter
+        /// property is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+    }
+}
